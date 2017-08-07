6e3fa710da05ba8386a953853d539e90c4247a21
sukanta <mailsam1979@gmail.com>
2017-08-07 15:57:16 +0100

    added maven project

---
+++ src/test/java/helloworld/SampleTest.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		
+		int a = 1;
+		int b = 2;
+		
+		assertTrue(a + b == 3);
+		
+	}
+
+}
